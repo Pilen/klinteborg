752c9799886368630aa0f15b4c2a7316eeacfa4c
Pilen <fiskomaten@gmail.com>
2023-10-19 21:42:43 +0200

    add cli command to load json with default values

--- backend/tasks/cli.py
+++ backend/tasks/cli.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import sys
+from pathlib import Path
 from backend.database import TX, DB
 from backend.api.user import make_login
 
@@ -16,6 +18,15 @@ def invite_command(tx: TX, args: argparse.Namespace) -> None:
     #     sys.exit(1)
     # backend.deltagere.api.user.make_invitation(tx, fdfid)
 
+
+def load_json_command(tx: TX, args: argparse.Namespace) -> None:
+    path = args.file
+    data = json.loads(path.read_text())
+    for table, rows in data.items():
+        print(f"Inserting into {table}")
+        for row in rows:
+            tx.insert_row(table, row)
+
 def main() -> None:
     # print(sys.argv)
     parser = argparse.ArgumentParser(
@@ -28,6 +39,11 @@ def main() -> None:
     parser_invite.add_argument("--fdfid", required=False, type=int, help="The fdfid of the user to invite")
     parser_invite.add_argument("--email", type=str, help="The email of the user to invite, if not specified the users email is used")
     parser_invite.add_argument("--print", action="store_true", help="Print the login link")
+
+    parser_load_json = subparsers.add_parser("load-json", help="Load json into the database")
+    parser_load_json.set_defaults(command_func=load_json_command)
+    parser_load_json.add_argument("--file", required=True, type=Path, help="Path to the json file")
+
     args = parser.parse_args()
     with DB() as db:
         with db.transaction() as tx:
